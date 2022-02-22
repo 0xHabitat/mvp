56edbd2c2bab31227fd881e7cacf0288335ede19
roleengineer <roleengineer@gmail.com>
2022-02-22 17:17:59 +0300

    stakeContract removed admin and extra check

--- contracts/external/VotingPowerManager.sol
+++ contracts/external/VotingPowerManager.sol
@@ -8,17 +8,17 @@ import { ITreasuryVotingPower } from "../interfaces/treasury/ITreasuryVotingPowe
 contract StakeContract {
   using SafeERC20 for IERC20;
 
-  address admin;
   ITreasuryVotingPower votingPower;
+  uint precision;
   // governanceToken => coefficient
   mapping(address => uint256) tokensMultiplier;
   // holder => govToken => staked amount
   mapping(address => mapping(address => uint)) stakedHoldings;
 
-  constructor(address _admin, address _votingPower, address[] memory _governanceTokens, uint256[] memory coefficients) {
+  constructor(address _votingPower, uint _precision, address[] memory _governanceTokens, uint256[] memory coefficients) {
     require(_votingPower != address(0)); // diamond address
     require(_governanceTokens.length == coefficients.length, "Different arrays length");
-    admin = _admin;
+    precision = _precision;
     votingPower = ITreasuryVotingPower(_votingPower);
     for (uint i = 0; i < _governanceTokens.length; i++) {
       tokensMultiplier[_governanceTokens[i]] = coefficients[i]; // coefficient - 1000 = 1.0
@@ -34,17 +34,15 @@ contract StakeContract {
     stakedHoldings[msg.sender][_governanceToken] += _amount;
     // give voting power
     uint multiplier = tokensMultiplier[_governanceToken];
-    uint amountOfVotingPower = _amount * multiplier / 1000;
+    uint amountOfVotingPower = _amount * multiplier / precision;
     votingPower.increaseVotingPower(msg.sender, amountOfVotingPower);
   }
 
   function unstake(address _governanceToken, uint _amount) public {
-    bool notAbleToUnstake = votingPower.hasVotedInActiveProposals(msg.sender);
-    require(!notAbleToUnstake, "Not able to unstake at the moment");
     require(stakedHoldings[msg.sender][_governanceToken] >= _amount, "Trying to unstake more than have");
     // take back voting power
     uint multiplier = tokensMultiplier[_governanceToken];
-    uint amountOfVotingPower = _amount * multiplier / 1000;
+    uint amountOfVotingPower = _amount * multiplier / precision;
     votingPower.decreaseVotingPower(msg.sender, amountOfVotingPower);
     // reduce token holdings
     stakedHoldings[msg.sender][_governanceToken] -= _amount;
@@ -70,38 +68,6 @@ contract StakeContract {
       unstake(_governanceTokens[i], _amounts[i]);
     }
   }
-  // Admin stuff
-
-  function addGovernanceToken(address _governanceToken, uint256 coefficient) external {
-    require(msg.sender == admin, "No permission");
-    tokensMultiplier[_governanceToken] = coefficient;
-  }
-
-  function removeGovernanceToken(address _governanceToken) public {
-    require(msg.sender == admin, "No permission");
-    require(IERC20(_governanceToken).balanceOf(address(this)) == 0, "All tokens must be unstaked.");
-    tokensMultiplier[_governanceToken] = 0;
-  }
-
-  function forceUnstakeGovernanceToken(address _governanceToken, address[] memory holders) public {
-    require(msg.sender == admin, "No permission");
-    for (uint i = 0; i < holders.length; i++) {
-      address holder = holders[i];
-      uint amount = stakedHoldings[holder][_governanceToken];
-      // take back voting power
-      uint multiplier = tokensMultiplier[_governanceToken];
-      uint amountOfVotingPower = amount * multiplier / 1000;
-      votingPower.decreaseVotingPower(msg.sender, amountOfVotingPower);
-      // send tokens to holder
-      IERC20(_governanceToken).safeTransfer(holder, amount);
-    }
-  }
-
-  function forceRemoveGovernanceToken(address _governanceToken, address[] memory holders) external {
-    forceUnstakeGovernanceToken(_governanceToken, holders);
-    removeGovernanceToken(_governanceToken);
-  }
-
 
 }
 
