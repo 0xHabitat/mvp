81373390158c767e429cf231bf55cd24b17a3032
Johann Barbie <johannbarbie@me.com>
2022-04-26 13:02:30 +0200

    hardhat and typescript config

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # mvp
+
+## setup
+
+```
+yarn
+yarn build
+```
+
+## test
+
+```
+yarn test
+```
+
+## deploy
+
+in separate terminal:
+
+```
+yarn run hardhat node
+```
+
+then in main terminal:
+
+```
+yarn deploy
+```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,14 @@
+import { task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
+import '@nomiclabs/hardhat-waffle';
 
-/* global ethers task */
-require('@nomiclabs/hardhat-waffle')
+// This adds support for typescript paths mappings
+import "tsconfig-paths/register";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async () => {
-  const accounts = await ethers.getSigners()
+task('accounts', 'Prints the list of accounts', async (args, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
     console.log(account.address)
@@ -15,15 +18,15 @@ task('accounts', 'Prints the list of accounts', async () => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+const config = {
   solidity: '0.8.9',
   settings: {
     optimizer: {
       enabled: true,
       runs: 200
     }
-  }
+  },
+  HardhatUserConfig: {}
 }
+
+export default config;
