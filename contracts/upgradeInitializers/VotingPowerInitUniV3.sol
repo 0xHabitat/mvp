742fc949086001ff9ca16fd24143f3799482847c
roleengineer <roleengineer@gmail.com>
2022-08-06 16:54:44 +0300

    added deploy pool to initUniV3

--- contracts/upgradeInitializers/VotingPowerInitUniV3.sol
+++ contracts/upgradeInitializers/VotingPowerInitUniV3.sol
@@ -5,6 +5,15 @@ import {LibVotingPower} from "../libraries/LibVotingPower.sol";
 import {IVotingPower} from "../interfaces/IVotingPower.sol";
 import {StakeContractERC20UniV3} from "../external/VotingPowerManagerERC20UniV3.sol";
 
+interface INFPositionManagerPoolDeploy {
+  function createAndInitializePoolIfNecessary(
+    address token0,
+    address token1,
+    uint24 fee,
+    uint160 sqrtPriceX96
+  ) external payable returns(address pool);
+}
+
 contract VotingPowerInitUniV3 {
   event VotingPowerManagerCreated(
     address indexed votingPowerManager,
@@ -23,6 +32,37 @@ contract VotingPowerInitUniV3 {
       address(this),
       _nfPositionManager,
       _governanceToken,
+      _legalPairTokens
+    );
+    vp.votingPowerManager = address(stakeContract);
+    emit VotingPowerManagerCreated(address(stakeContract), address(this));
+  }
+
+  // prerequisites: ERC20Facet is attached and diamond address is governanceToken
+  // sqrtPriceX96 must be set for the pairToken at index = 0
+  function initVotingPowerERC20UniV3DeployMainPools(
+    uint256 _maxAmountOfVotingPower,
+    address _nfPositionManager,
+    address[] memory _legalPairTokens,
+    uint160 _sqrtPriceX96
+  ) external {
+    IVotingPower.VotingPower storage vp = LibVotingPower.votingPowerStorage();
+    vp.maxAmountOfVotingPower = _maxAmountOfVotingPower;
+
+    if (address(this) < _legalPairTokens[0]) {
+      INFPositionManagerPoolDeploy(_nfPositionManager).createAndInitializePoolIfNecessary(address(this), _legalPairTokens[0], uint24(500), _sqrtPriceX96);
+      INFPositionManagerPoolDeploy(_nfPositionManager).createAndInitializePoolIfNecessary(address(this), _legalPairTokens[0], uint24(3000), _sqrtPriceX96);
+      INFPositionManagerPoolDeploy(_nfPositionManager).createAndInitializePoolIfNecessary(address(this), _legalPairTokens[0], uint24(10000), _sqrtPriceX96);
+    } else {
+      INFPositionManagerPoolDeploy(_nfPositionManager).createAndInitializePoolIfNecessary(_legalPairTokens[0], address(this), uint24(500), _sqrtPriceX96);
+      INFPositionManagerPoolDeploy(_nfPositionManager).createAndInitializePoolIfNecessary(_legalPairTokens[0], address(this), uint24(3000), _sqrtPriceX96);
+      INFPositionManagerPoolDeploy(_nfPositionManager).createAndInitializePoolIfNecessary(_legalPairTokens[0], address(this), uint24(10000), _sqrtPriceX96);
+    }
+
+    StakeContractERC20UniV3 stakeContract = new StakeContractERC20UniV3(
+      address(this),
+      _nfPositionManager,
+      address(this),
       _legalPairTokens
     );
     vp.votingPowerManager = address(stakeContract);
