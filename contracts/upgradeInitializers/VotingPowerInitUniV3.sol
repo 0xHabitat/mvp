0fac701de6d766a5c7577ad212a787c1dd6ef786
roleengineer <roleengineer@gmail.com>
2022-08-26 13:52:08 +0300

    edited initTreasury

--- contracts/upgradeInitializers/VotingPowerInitUniV3.sol
+++ contracts/upgradeInitializers/VotingPowerInitUniV3.sol
@@ -25,12 +25,14 @@ contract VotingPowerInitUniV3 {
   );
 
   function initVotingPowerERC20UniV3(
+    uint256 _precision,
     uint256 _maxAmountOfVotingPower,
     address _nfPositionManager,
     address _governanceToken,
     address[] memory _legalPairTokens
   ) external {
     IVotingPower.VotingPower storage vp = LibVotingPower.votingPowerStorage();
+    vp.precision = _precision;
     vp.maxAmountOfVotingPower = _maxAmountOfVotingPower;
     StakeContractERC20UniV3 stakeContract = new StakeContractERC20UniV3(
       address(this),
@@ -45,11 +47,13 @@ contract VotingPowerInitUniV3 {
   // prerequisites: ERC20Facet is attached and diamond address is governanceToken
   // sqrtPriceX96 must be set for the pairToken at index = 0
   function initVotingPowerERC20UniV3DeployMainPools(
+    uint256 _precision,
     address _nfPositionManager,
     address[] memory _legalPairTokens,
     uint160 _sqrtPriceX96
   ) external {
     IVotingPower.VotingPower storage vp = LibVotingPower.votingPowerStorage();
+    vp.precision = _precision;
     vp.maxAmountOfVotingPower = IERC20(address(this)).totalSupply();
 
     if (address(this) < _legalPairTokens[0]) {
